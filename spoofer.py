7b710313630c64777edebe57a510f5107b710e57
ofirshitrit <ofirha18@gmail.com>
2023-06-07 09:15:47 +0300

    working on Spoofer

--- spoofer.py
+++ spoofer.py
@@ -1,66 +1,44 @@
-import socket
-import struct
-import select
+import sys
+from scapy.all import  send
+from scapy.layers.inet import ICMP, UDP, TCP, IP
 
 
-def checksum(msg):
-    s = 0
-    n = len(msg) % 2
-    for i in range(0, len(msg) - n, 2):
-        s += (msg[i] << 8) + msg[i + 1]
-    if n:
-        s += msg[i + 1] << 8
-    while (s >> 16):
-        s = (s & 0xFFFF) + (s >> 16)
-    s = ~s & 0xffff
-    return s
+def send_raw_ip_packet(packet):
+    send(packet, verbose=False)
 
 
-def spoof_icmp_packet(src, dst):
-    packet = struct.pack("!BBHHH", 8, 0, 0, 1, 1)
-    chksum = checksum(packet)
-    packet = struct.pack("!BBHHH", 8, 0, socket.htons(chksum), 1, 1)
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-    s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-    ip_header = struct.pack("!BBHHHBBH4s4s", 69, 0, len(packet), 0, 0, 64, socket.IPPROTO_ICMP, 0,
-                            socket.inet_aton(src), socket.inet_aton(dst))
-
-    # Send the packet
-    s.sendto(ip_header + packet, (dst, 0))
-
-    # Create a new socket to listen for responses
-    r = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-    r.bind(('', 0))
-    r.setblocking(0)
-
-    while True:
-        ready, _, _ = select.select([r], [], [], 1)
-        if ready:
-            response, addr = ready[0].recvfrom(4096)
-            print(f"Response: {response}, Addr: {addr}")
-        else:
-            print("No response within timeout, exiting...")
-            break
+def icmp():
+    ip_header = IP(src="1.2.3.4", dst="10.0.2.15")
+    icmp_packet = ICMP(type=8)  # ICMP Type: 8 is request, 0 is reply
+    packet = ip_header / icmp_packet
+    send_raw_ip_packet(packet)
 
 
-# def spoof_tcp_packet(src, dst):
-#
-#
-#     # similar to ICMP spoofing but with IPPROTO_TCP and changing the packet to be TCP
-#
-# def spoof_udp_packet(src, dst):
-#
-#
-#     # similar to ICMP spoofing but with IPPROTO_UDP and changing the packet to be UDP
+def udp():
+    ip_header = IP(src="10.0.2.15", dst="10.0.2.15")
+    udp_packet = UDP(sport=12345, dport=9090)
+    data = "Hello Server!\n"
+    packet = ip_header / udp_packet / data
+    send_raw_ip_packet(packet)
+
+
+def tcp():
+    ip_header = IP(src="1.2.3.4", dst="10.0.2.15")
+    tcp_packet = TCP(sport=12345, dport=9090, seq=1)
+    data = "Hello Server!\n"
+    packet = ip_header / tcp_packet / data
+    send_raw_ip_packet(packet)
+
 
 def main():
-    src = "1.2.3.4"
-    dst = "10.0.2.15"
-    spoof_icmp_packet(src, dst)
-    print("ICMP packet sent!")
-    # spoof_tcp_packet(src, dst)
-    # spoof_udp_packet(src, dst)
+    icmp()
+    print("ICMP sent")
+    #
+    # udp()
+    # print("UDP sent")
+    #
+    # tcp()
+    # print("TCP sent")
 
 
 if __name__ == "__main__":
